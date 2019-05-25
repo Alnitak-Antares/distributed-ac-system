6e42b8d593218033f9be8cfb829249e5e5949941
Macaulish64 <justfreefood@163.com>
2019-05-25 17:31:59 +0800

    update scheduler
    update scheduler

--- src/main/java/app/dto/RoomState.java
+++ src/main/java/app/dto/RoomState.java
@@ -1,8 +1,35 @@
 package app.dto;
 
 public class RoomState {
-    private int nowTemp;            //当然室温
-    private Boolean isPowerOn;          //
+    private int nowTemp;            //当前室温
+    private int tarTemp;            //最后一次的目标温度
+    private int funSpeed;           //最后一次的目标风速
+    private Boolean isPowerOn;      //是否开机
+    private Boolean isInService;    //是否服务
+
+    public int getTarTemp() {
+        return tarTemp;
+    }
+
+    public void setTarTemp(int tarTemp) {
+        this.tarTemp = tarTemp;
+    }
+
+    public int getFunSpeed() {
+        return funSpeed;
+    }
+
+    public void setFunSpeed(int funSpeed) {
+        this.funSpeed = funSpeed;
+    }
+
+    public Boolean getInService() {
+        return isInService;
+    }
+
+    public void setInService(Boolean inService) {
+        isInService = inService;
+    }
 
     public int getNowTemp() {
         return nowTemp;
@@ -19,4 +46,6 @@ public class RoomState {
     public void setPowerOn(Boolean powerOn) {
         isPowerOn = powerOn;
     }
+
+
 }
