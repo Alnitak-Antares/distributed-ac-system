c2a6b38bde8156611c6d66d4efc9085a3f41c123
Macaulish64 <justfreefood@163.com>
2019-05-25 16:51:48 +0800

    update
    update

--- src/main/java/app/service/scheduler.java
+++ src/main/java/app/service/scheduler.java
@@ -1,4 +1,30 @@
 package app.service;
 
+
+import app.dto.RoomState;
+import app.dto.Service;
+
+import java.util.ArrayList;
+
 public class scheduler {
+    private ArrayList<Service> waitingList;
+    private ArrayList<Service> runningList;
+    private ArrayList<RoomState> roomList;
+
+    //==========================================
+    //接收请求
+    //==========================================
+    //1. 房客
+
+    //
+    public Service findRoomService()
+    {
+        return null;
+    }
+
+    //初始化
+    public void schduler() {
+
+
+    }
 }
