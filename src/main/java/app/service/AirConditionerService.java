46d49b85ad33e25c83209f376c5aef7bf0e33192
Macaulish64 <justfreefood@163.com>
2019-05-29 16:37:13 +0800

    Revert "fix chaneg temp limit"
    This reverts commit 1d3995d32f6be2d5e4b6aedce2947012d7f0684a.

--- src/main/java/app/service/AirConditionerService.java
+++ src/main/java/app/service/AirConditionerService.java
@@ -373,8 +373,6 @@ public class AirConditionerService {
     private void timerToChangeRoomTemp() {
         for(Room nowRoom:roomList) {
             double nowRoomTemp=nowRoom.getNowTemp();
-            if (nowRoomTemp<acParams.getTempLowLimit()) continue;
-            if (nowRoomTemp>acParams.getTempHighLimit()) continue;
             if (nowRoom.isInService()) {
                 switch (nowRoom.getFunSpeed()) {
                     case "Low":nowRoom.setNowTemp(nowRoomTemp-0.1);break;
